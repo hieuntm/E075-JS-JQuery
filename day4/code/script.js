b00223ebe2381683ceb80e018577e2ee3d6ae659
hieuntm <hieuntm168@gmail.com>
2024-06-06 21:15:59 +0700

    Complete funcrtion

--- day4/code/script.js
+++ day4/code/script.js
@@ -1,14 +1,35 @@
-"use strict";
 let soDu = 1000;
 const lichSuTaiKhoan = []; // => Xem lich su nap tien/ rut tien
-const input = prompt("Xin mời nhập số tiền cần nạp");
-const soTien = +input; // + cho phép đổi thành số
+// const input = prompt("Xin mời nhập số tiền cần nạp");
+// const soTien = +input; // + cho phép đổi thành số
 // soTien sẽ là NaN nếu nhập chữ
-console.log("Ngoài: ", soTien, typeof soTien);
-if (!isNaN(soTien)) {
-    console.log("trong: ", soTien, typeof soTien);
+// console.log("Ngoài: ", soTien, typeof soTien);
+// if (!isNaN(soTien)) {
+//     console.log("trong: ", soTien, typeof soTien);
+// }
+
+//2. Check rút tiền
+function displayBalance() {
+    const p = document.getElementById("balance");
+    p.innerHTML = "Số dư: $" + soDu;
 }
 
+displayBalance();
+
+function displayHistory(number) {
+    const p = document.createElement("p");
+    if (number > 0) {
+        p.style.color = "green"
+        p.innerHTML = "Vừa nạp " + number + "$ tiền";
+    } else {
+        p.style.color = "red"
+        p.innerHTML = "Vừa rút " + +number + "$ tiền";
+    }
+
+    const div = document.getElementById("history");
+    div.appendChild(p);
+    displayBalance();
+}
 
 function napTien() {
     const soTien = prompt("Xin mời nhập số tiền cần nạp");
@@ -16,9 +37,10 @@ function napTien() {
     // Kiểm tra nó có phải số hay không => if (typeof soTien === Number)
     // lichSu => +soTien
 
-
-    lichSuTaiKhoan.push(+soTien);
+    lichSuTaiKhoan.unshift(+soTien);
+    soDu += +soTien;
     console.log(lichSuTaiKhoan)
+    displayHistory(+soTien)
 }
 
 function rutTien() {
@@ -29,9 +51,11 @@ function rutTien() {
     // Kiểm tra xem số dư có đủ hay không
 
     // lichSu => +soTien
-    lichSuTaiKhoan.push(-soTien);
-    console.log(lichSuTaiKhoan)
+    lichSuTaiKhoan.unshift(-soTien);
+    soDu -= +soTien;
 
+    console.log(lichSuTaiKhoan)
+    displayHistory(-soTien)
 }
 
 // console.log(text);
